3811ac474968dacb08836aecc40f373f6f0b51d5
KasiMKurtay <kasmkurtay@outlook.com>
2025-04-11 14:27:47 +0300

    Models has been created

---
+++ backend/src/lib/db.js
@@ -0,0 +1,12 @@
+import { log } from "console";
+import mongoose from "mongoose";
+
+export const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGODB_URI);
+    console.log(`Connected to MongoDB ${conn.connection.host}`);
+  } catch (error) {
+    console.log("Failed to coonect to MongoDB", error);
+    process.exit(1); //1 hata mesajı , 0 doğru
+  }
+};
