9c2451fc6a28dba77618e7cdbcc19f6a57c19f5d
KasiMKurtay <kasmkurtay@outlook.com>
2025-04-11 16:16:24 +0300

    TODO: backend boilerplate has been completed

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import { clerkMiddleware } from "@clerk/express";
 
 import adminRoutes from "./routes/admin.route.js";
 import userRoutes from "./routes/user.route.js";
@@ -15,6 +16,8 @@ const app = express();
 const PORT = process.env.PORT;
 
 app.use(express.json());
+
+app.use(clerkMiddleware());
 
 app.use("/api/users", userRoutes);
 app.use("/api/auth", authRoutes);
--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -3,22 +3,8 @@ import { Router } from "express";
 const router = Router();
 
 router.get("/", (req, res) => {
+  req.auth.userId;
   res.send("User route with get");
 });
 
-router.post("/", (req, res) => {
-  res.send("User route with get");
-});
-
-
-router.delete("/", (req, res) => {
-  res.send("User route with get");
-});
-
-
-router.get("/", (req, res) => {
-  res.send("User route with get");
-});
-
-
-export default router
+export default router;
